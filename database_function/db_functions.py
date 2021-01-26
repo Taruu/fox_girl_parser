6cdc7faeb39374385f52c96dec68bad71a8a409d
Taruu <markov2002@bk.ru>
2021-01-26 22:20:06 +0300

    add tulpi add_obj

--- database_function/db_functions.py
+++ database_function/db_functions.py
@@ -8,8 +8,14 @@ class DatabaseWorker:
     def __init__(self):
         self.sessionWorker = ImageDatabase()
 
-    def add_object(self,md5_hash:str, rating:str, tags:list, urls_image:list, file_format=None):
-        object_image = self.sessionWorker.Object(md5_hash=md5_hash, rating=rating)
-        object_image.tags
+    def add_object(self,
+                   md5_hash:str,
+                   rating:str, tags:list, urls_image:list, file_format=None,
+                   file_size=0,
+                   image_weight=0,
+                   image_height=0):
+        print(md5_hash)
+        #object_image = self.sessionWorker.Object(md5_hash=md5_hash, rating=rating)
+        #object_image.tags
 
 
