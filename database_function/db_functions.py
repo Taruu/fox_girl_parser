fe0302647819b8c94bb63ddde156303ad0ba8e89
Taruu <markov2002@bk.ru>
2021-01-26 22:23:11 +0300

    edit tulp

--- database_function/db_functions.py
+++ database_function/db_functions.py
@@ -8,12 +8,7 @@ class DatabaseWorker:
     def __init__(self):
         self.sessionWorker = ImageDatabase()
 
-    def add_object(self,
-                   md5_hash:str,
-                   rating:str, tags:list, urls_image:list, file_format=None,
-                   file_size=0,
-                   image_weight=0,
-                   image_height=0):
+    def add_object(self,md5_hash:str,rating:str, tags:list, urls_image:list):
         print(md5_hash)
         #object_image = self.sessionWorker.Object(md5_hash=md5_hash, rating=rating)
         #object_image.tags
