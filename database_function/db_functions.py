9ad8994f541eb4bef555a58977afac720efe7e84
Taruu <markov2002@bk.ru>
2021-01-27 23:22:39 +0300

    Update db_functions.py
    add desc

--- database_function/db_functions.py
+++ database_function/db_functions.py
@@ -9,6 +9,17 @@ class DatabaseWorker:
         self.sessionWorker = ImageDatabase()
 
     def add_object(self,md5_hash:str,rating:str, tags:list, urls_image:list):
+        """
+        md5_hash - The largest image hash available
+        rating - image rating s,q и еще че то
+        tags - Tags pictures
+        urls_image -Image storage links
+        How insert urls?
+        Exsemple:
+        {"size": 1,"width":22,"height":33,"file_format":"jpg","url":"https://pbs.twimg.com/media/EGbhF6TVAAEEHdy.jpg"}
+        size - bytes
+        width and height - px
+        """
         print(md5_hash)
         #object_image = self.sessionWorker.Object(md5_hash=md5_hash, rating=rating)
         #object_image.tags
