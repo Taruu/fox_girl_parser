a2412475bb3b4db0237d5fa3285d4663d9bd349d
andrKonan <52403327+andrKonan@users.noreply.github.com>
2021-01-28 12:41:42 +0100

    [SC] get_size, first try

--- utilities/get_size.py
+++ utilities/get_size.py
@@ -2,42 +2,47 @@ import requests
 from PIL import ImageFile
 
 class GetSize():
-    def __init__(self, uri: str = None):
-        self.uri = uri
+    def __init__(self, url: str = None, disable_url_check = False):
+        self.disable_url_check = disable_url_check or False
+        self.change_url(url)
 
-    @staticmethod
-    def check_uri(uri):
-        if isinstance(uri, str):
-            if (uri.startswith("https://") or uri.startswith("http://")) and (uri.endswith(".png") or uri.endswith(".jpg") or uri.endswith(".jpeg") or uri.endswith(".webp")):
+    def check_url(self, url, disable_url_check = False):
+        if disable_url_check:
+            return True
+
+        if isinstance(url, str):
+            if (url.startswith("https://") or url.startswith("http://")) and (url.endswith(".png") or url.endswith(".jpg") or url.endswith(".jpeg") or url.endswith(".webp")):
                 return True
             else:
                 return False
         else:
             return False
 
-    def change_uri(self, uri: str):
-        self.uri = uri
+    def change_url(self, url: str, disable_url_check = False):
+        if not self.check_url(url, disable_url_check):
+            self.url = url
+        else:
+            raise ValueError("Wrong URI, to disable use disable_url_check flag")
 
+    def get_size(self, url = None, disable_url_check = False):
+        if not self.check_url(url, disable_url_check):
+            raise ValueError("Wrong URI, to disable use disable_url_check flag")
 
+        requesting_file = requests.get(url, stream=True)
+        size = requesting_file.headers.get("content-length")
 
-# def getsizes(uri):
-#     # get file size *and* image size (None if not known)
-#     file = requests.get(uri, stream=True)
-#     size = file.headers.get("content-length")
-#     if size:
-#         size = int(size)
-#     p = ImageFile.Parser()
-#     while 1:
-#         data = file.raw.read(1024)
-#         if not data:
-#             break
-#         p.feed(data)
-#         if p.image:
-#             return size, p.image.size
-#             break
-#     file.close()
-#     return size, None
-#
-#
-#
-# print(getsizes("https://danbooru.donmai.us/data/a020846a0b3068986b228e0f6c2d8342.png"))
+        size = int(size) or size
+        image_parser = ImageFile.Parser()
+
+        while True:
+            data = requesting_file.raw.read(1024)
+            if not data:
+                requesting_file.close()
+                return size, None
+
+            image_parser.feed(data)
+            if image_parser.image:
+                requesting_file.close()
+                return size, image_parser.image.size
+gs = GetSize()
+print(gs.get_size("https://danbooru.donmai.us/data/a020846a0b3068986b228e0f6c2d8342.png"))
