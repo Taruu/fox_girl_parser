27aa6764b8ff06fcb0b642cade7de3f3ab14e7a1
andrKonan <52403327+andrKonan@users.noreply.github.com>
2021-01-28 11:17:33 +0100

    Rename and some getsize

---
+++ utilities/get_size.py
@@ -0,0 +1,43 @@
+import requests
+from PIL import ImageFile
+
+class GetSize():
+    def __init__(self, uri: str = None):
+        self.uri = uri
+
+    @staticmethod
+    def check_uri(uri):
+        if isinstance(uri, str):
+            if (uri.startswith("https://") or uri.startswith("http://")) and (uri.endswith(".png") or uri.endswith(".jpg") or uri.endswith(".jpeg") or uri.endswith(".webp")):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def change_uri(self, uri: str):
+        self.uri = uri
+
+
+
+# def getsizes(uri):
+#     # get file size *and* image size (None if not known)
+#     file = requests.get(uri, stream=True)
+#     size = file.headers.get("content-length")
+#     if size:
+#         size = int(size)
+#     p = ImageFile.Parser()
+#     while 1:
+#         data = file.raw.read(1024)
+#         if not data:
+#             break
+#         p.feed(data)
+#         if p.image:
+#             return size, p.image.size
+#             break
+#     file.close()
+#     return size, None
+#
+#
+#
+# print(getsizes("https://danbooru.donmai.us/data/a020846a0b3068986b228e0f6c2d8342.png"))
