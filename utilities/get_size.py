78d79f3a49e857906c1789a764f0c3981af46edc
andrKonan <52403327+andrKonan@users.noreply.github.com>
2021-01-28 14:42:02 +0100

    get_size, almost finished

--- utilities/get_size.py
+++ utilities/get_size.py
@@ -2,47 +2,37 @@ import requests
 from PIL import ImageFile
 
 class GetSize():
-    def __init__(self, url: str = None, disable_url_check = False):
-        self.disable_url_check = disable_url_check or False
-        self.change_url(url)
+    def __init__(self, url: str = None):
+        self.url = url
 
-    def check_url(self, url, disable_url_check = False):
-        if disable_url_check:
-            return True
+    def change_url(self, url: str = None):
+        self.url = url
 
-        if isinstance(url, str):
-            if (url.startswith("https://") or url.startswith("http://")) and (url.endswith(".png") or url.endswith(".jpg") or url.endswith(".jpeg") or url.endswith(".webp")):
-                return True
-            else:
-                return False
-        else:
-            return False
+    def get_size(self):
+        url = self.url
 
-    def change_url(self, url: str, disable_url_check = False):
-        if not self.check_url(url, disable_url_check):
-            self.url = url
-        else:
-            raise ValueError("Wrong URI, to disable use disable_url_check flag")
+        if not isinstance(url, str):
+            raise ValueError("The url is not leads to an image")
 
-    def get_size(self, url = None, disable_url_check = False):
-        if not self.check_url(url, disable_url_check):
-            raise ValueError("Wrong URI, to disable use disable_url_check flag")
+        if url.startswith("https://") or url.startswith("http://"):
+            requesting_file = requests.get(url, stream=True)
+            if requesting_file.headers.get("Content-Type").startswith("image/"):
+                size = requesting_file.headers.get("Content-Length")
+                size = int(size) or size
+                image_parser = ImageFile.Parser()
 
-        requesting_file = requests.get(url, stream=True)
-        size = requesting_file.headers.get("content-length")
+                while True:
+                    data = requesting_file.raw.read(1024)
+                    if not data:
+                        requesting_file.close()
+                        return size, None
 
-        size = int(size) or size
-        image_parser = ImageFile.Parser()
+                    image_parser.feed(data)
+                    if image_parser.image:
+                        requesting_file.close()
+                        return size, image_parser.image.size
 
-        while True:
-            data = requesting_file.raw.read(1024)
-            if not data:
-                requesting_file.close()
-                return size, None
+        raise ValueError("The url is not leads to an image")
 
-            image_parser.feed(data)
-            if image_parser.image:
-                requesting_file.close()
-                return size, image_parser.image.size
-gs = GetSize()
-print(gs.get_size("https://danbooru.donmai.us/data/a020846a0b3068986b228e0f6c2d8342.png"))
+gs = GetSize("https://danbooru.donmai.us/data/a020846a0b3068986b228e0f6c2d8342.png")
+print(gs.get_size())
