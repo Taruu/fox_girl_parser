489739731b621494eda645347d136fab5802b747
Taruu <markov2002@bk.ru>
2021-01-31 20:19:54 +0300

    time table

--- database/fox_girl.sql
+++ database/fox_girl.sql
@@ -17,6 +17,13 @@ CREATE TABLE `tag` (
   `name` VARCHAR(255) NOT NULL
 );
 
+
+
+CREATE TABLE `time_file` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `time_file` timestamp UNIQUE NOT NULL
+);
+
 CREATE TABLE `file_url` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `id_object` int NOT NULL,
@@ -25,6 +32,8 @@ CREATE TABLE `file_url` (
   `url` longTEXT NOT NULL,
   `hash_url` varchar(255) UNIQUE NOT NULL,
   `file_ext` char(8) NOT NULL
+  `id_update_at` int NOT NULL,
+  `id_create_at` int NOT NULL
 );
 
 ALTER TABLE object ADD UNIQUE (md5_hash);
@@ -33,8 +42,15 @@ ALTER TABLE tag ADD UNIQUE (name);
 
 ALTER TABLE file_url ADD UNIQUE (hash_url);
 
+ALTER TABLE time_file ADD UNIQUE (time_file);
+
 ALTER TABLE `obj_to_tag` ADD FOREIGN KEY (`tag`) REFERENCES `tag` (`id`);
 
 ALTER TABLE `obj_to_tag` ADD FOREIGN KEY (`object`) REFERENCES `object` (`id`);
 
 ALTER TABLE `file_url` ADD FOREIGN KEY (`id_object`) REFERENCES `object` (`id`);
+
+
+ALTER TABLE `file_url` ADD FOREIGN KEY (`id_update_at`) REFERENCES `time_file` (`id`);
+
+ALTER TABLE `file_url` ADD FOREIGN KEY (`id_create_at`) REFERENCES `time_file` (`id`);
