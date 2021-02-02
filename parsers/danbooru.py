166a165457cc85ae3f9bed51f0a162f72eadec84
andrKonan <52403327+andrKonan@users.noreply.github.com>
2021-02-02 15:41:26 +0100

    Danbooru, better get_posts date search

--- parsers/danbooru.py
+++ parsers/danbooru.py
@@ -5,9 +5,8 @@ from pybooru import Danbooru as booru
 from utilities.ImageTools import ImageTools
 
 class Danbooru():
-
     @staticmethod
-    def get_posts(tags = "", page = 0, limit = 1000, date_start = None, date_end = None, filter_bad_images = True):
+    def get_posts(tags = "", page = 0, limit = 1000, date_start = None, date_end = None, date_year = None, date_one_year = True, filter_bad_images = True):
         limit = limit if limit < 1000 else 1000
 
         if isinstance(date_start, datetime.datetime) and isinstance(date_end, datetime.datetime):
@@ -15,6 +14,11 @@ class Danbooru():
                 tags += " date:" + date_start.strftime("%Y-%m-%d") + ".." + date_end.strftime("%Y-%m-%d")
             else:
                 raise ValueError("date_start must be earlier than date_end")
+        elif isinstance(date_start, datetime.datetime) and bool(date_one_year):
+            tags += " date:" + date_start.strftime("%Y-%m-%d") + ".." + (date_start + datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+        elif isinstance(date_year, int) and date_year > 1970 and date_year <= datetime.date.today().year:
+            tags += " date:" + datetime.datetime(date_year, 1, 1).strftime("%Y-%m-%d") + ".." + datetime.datetime(date_year + 1, 1, 1).strftime("%Y-%m-%d")
+
 
         client = booru('danbooru')
 
@@ -35,4 +39,4 @@ class Danbooru():
                 })
         return res
 
-# print(Danbooru.get_posts(tags = "fox_ears", page = 1, date_start = datetime.datetime(2012, 1, 1), date_end = datetime.datetime(2013, 1, 1), filter_bad_images = True))
+print(Danbooru.get_posts(tags = "fox_ears", page = 1, date_year = 2012, filter_bad_images = True))
