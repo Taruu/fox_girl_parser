3d6e7d42b0421991ba36efaf62c22d9db27335e2
Taruu <markov2002@bk.ru>
2021-02-07 14:59:10 +0300

    fix TextTools.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ dp = DanbooruParser()
 datebase = DatabaseWorker()
 
 tag_list = ["fox_tail","fox_ears",]
-tag = "fox_tail"
+tag = "fox_girl"
 if os.path.exists(f"{tag}.json"):
     with open(f"{tag}.json") as file:
         tag_q = json.load(file)
@@ -21,7 +21,8 @@ if tag_q[0]:
         posts = dp.get_posts(tags=tag, page=page)
         for i, object in enumerate(posts):
             #print(i,object["md5"])
-            timestamp = datetime.datetime(object["created_at"].year,object["created_at"].month,object["created_at"].day)
+            created_at = datetime.datetime.fromisoformat(object["created_at"])
+            timestamp = datetime.datetime(created_at.year,created_at.month,created_at.day)
             result = datebase.add_object(object["md5"],object["rating"],int(timestamp.timestamp()),object["file_size"],object["width"],object["height"],object['tags'],object["urls"])
             if result[0] == "Obj_exists":
                 datebase.update_object(result[1],object["rating"],int(timestamp.timestamp()),object["tags"],object["urls"])
